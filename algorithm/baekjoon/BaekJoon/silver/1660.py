b780cfdb0f2c87c31b68f936b5cd566e77e30838
wns312 <wns312@gmail.com>
2022-02-02 15:53:11 +0900

    bj 1160

--- algorithm/baekjoon/BaekJoon/silver/1660.py
+++ algorithm/baekjoon/BaekJoon/silver/1660.py
@@ -4,26 +4,23 @@ sys.stdin = open('input.txt', 'r')
 N = int(input())
 arr = [1]
 memo = [1]
-for i in range(2, 10000000):
-    if memo[-1] > N:
-        break
 
+for i in range(2, 10000000):
+    if arr[-1]+i + memo[-1] > N:
+        break
     arr.append(arr[-1]+i)
     memo.append(arr[-1]+memo[-1])
 
-dp = [0] * len(memo)
-# 동전 문제를 먼저 풀어본 뒤 돌아오자.
-# 여기 최소 사면체 개수 구하는 부분이 잘못됐음.
-# ex) 70일 때 35, 20, 20, 4로 4개가 정답임. -> 어떻게 구할것인가?
-for i in range(len(memo)-1, -1, -1):
-    remain = N
-    count = 0
-    for j in range(i, -1, -1):
-        count += remain // memo[j]
-        remain = remain % memo[j]
 
-    dp[i] = count
-# 여기 위까지 고쳐야함
-print(arr)
-print(memo)
-print(dp)
+dp = [-1] * (N+1)
+dp[0] = 0
+
+for i in range(1, (N+1)):
+    l = []
+    for cnt in memo:
+        if i-cnt >= 0 and dp[i-cnt] != -1:
+            l.append(dp[i-cnt])
+    if l:
+        dp[i] = min(l)+1
+
+print(dp[-1])
