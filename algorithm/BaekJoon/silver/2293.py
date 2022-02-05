c4ef6550eae55b6f0877ff2478053c1c60d710f5
wns312 <wns312@gmail.com>
2022-02-06 02:12:18 +0900

    bj 2293

---
+++ algorithm/BaekJoon/silver/2293.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+N, K = map(int, input().split())
+coins = [int(input()) for _ in range(N)]
+dp = [0] * (K+1)
+dp[0] = 1
+for coin in coins:
+    for i in range(1, K+1):
+        if i >= coin:
+            dp[i] += dp[i-coin]
+print(dp[-1])
+
