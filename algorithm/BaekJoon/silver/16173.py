faa2479694d71cbaae09e08685bc688c1ca07aaa
wns312 <wns312@gmail.com>
2022-04-08 20:57:16 +0900

    bj 16173

---
+++ algorithm/BaekJoon/silver/16173.py
@@ -0,0 +1,26 @@
+import sys
+from queue import PriorityQueue
+sys.stdin = open('../gold/input.txt', 'r')
+
+
+import sys
+sys.setrecursionlimit(250000)
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+result = 'Hing'
+def dfs(r, c):
+    global arr, result, N
+    if not arr[r][c]:
+        return
+    if r == N-1 and c == N-1:
+        result = 'HaruHaru'
+        return
+    move = arr[r][c]
+    if r+move < N:
+        dfs(r+move, c)
+    if c + move < N:
+        dfs(r, c+move)
+
+dfs(0, 0)
+print(result)
+
