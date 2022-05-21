7b228791e3d295635601b0fa4bb8080b54d09199
wns312 <wns312@gmail.com>
2022-05-21 21:18:21 +0900

    bj 1359(직접구현)

--- algorithm/BaekJoon/silver/1359.py
+++ algorithm/BaekJoon/silver/1359.py
@@ -1,7 +1,22 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-from itertools import permutations
+
+# p_count: 뽑은 카운트, d_count: 목표로 하는 뽑기 카운트
+# dc_count: 겹쳐야 하는 개수
+def perm(picked, p_count, d_count, dc_count):
+    if p_count == d_count:
+        if picked[:d_count].count(True) >= dc_count:
+            return 1
+        return 0
+
+    count = 0
+    for i in range(len(picked)):
+        if not picked[i]:
+            picked[i] = True
+            count += perm(picked, p_count+1, d_count, dc_count)
+            picked[i] = False
+    return count
 
 
 N, M, K = map(int, input().split())
@@ -10,16 +25,8 @@ entire_choice = 1
 for i in range(N, N-M, -1):
     entire_choice *= i
 
-arr = [i+1 for i in range(N)] # 전체 배열
-picked = arr[:M]  # 뽑은 개수
-r = list(permutations(arr, M)) # 전체 배열에서 M개 뽑은 리스트
-r_count = 0
+picked = [False] * N
 
-for i in range(len(r)):
-    count = 0
-    for p in picked:
-        if p in r[i]:
-            count += 1
-    if count >= K:
-        r_count += 1
+r_count = perm(picked, 0, M, K)
+
 print(r_count/entire_choice)
